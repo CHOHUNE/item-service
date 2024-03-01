a3a40a50b8a8e86e6748f1db590420beef5833bc
CHOHUNE <yuureru@gmail.com>
2024-03-01 11:01:41 +0900

    상품 목록- 타임리프 기능 설명

---
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -0,0 +1,44 @@
+package hello.itemservice.web;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "/basic/items";
+    }
+
+//
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemA", 20000, 10));
+    }
+
+
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+//    배운 내용 리마인드 -> 스프링에서는 생성자가 한개면 @Autowired 생략 가능
+//    추가로 lombok을 이용하여 RequiredArgsContructor 을 이용
+//    어노테이션 하나로 간단히 생성자 주입 완료
+}
